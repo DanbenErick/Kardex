8c19db4ed4034c44056688cc56c8101c366622c8
Danben Erick <danbencb08@outlook.es>
2021-01-09 23:05:05 -0500

    Termine los handlers del formulario

--- src/js/components/Formulario.jsx
+++ src/js/components/Formulario.jsx
@@ -1,31 +1,119 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
-const Formulario = () => {
+const Formulario = (props) => {
+
+  const [ kardex, setKardex ] = useState({
+    fecha: '',
+    detalle: '',
+    valor_unitario: '',
+    opcion: '',
+    cantidad: ''
+  })
+
+  const handleChangeDate = (event) => {
+    setKardex({
+      fecha: event.target.value,
+      detalle: kardex.detalle || '',
+      valor_unitario: kardex.valor_unitario || '',
+      opcion: kardex.opcion || '',
+      cantidad: kardex.cantidad || ''
+    })
+  }
+
+  const handleChangeDetails = (event) => {
+    setKardex({
+      fecha: kardex.fecha || '',
+      detalle: event.target.value,
+      valor_unitario: kardex.valor_unitario || '',
+      opcion: kardex.opcion || '',
+      cantidad: kardex.cantidad || ''
+    })
+  }
+
+  const handleChangeValueUnitary = (event) => {
+    setKardex({
+      fecha: kardex.fecha || '',
+      detalle: kardex.detalle,
+      valor_unitario: event.target.value,
+      opcion: kardex.opcion || '',
+      cantidad: kardex.cantidad || ''
+    })
+  }
+
+  const handleChangeOption = (event) => {
+    setKardex({
+      fecha: kardex.fecha || '',
+      detalle: kardex.detalle || '',
+      valor_unitario: kardex.valor_unitario || '',
+      opcion: event.target.value,
+      cantidad: kardex.cantidad || ''
+    })
+  }
+
+  const handleChangeQuantity = (event) => {
+    setKardex({
+      fecha: kardex.fecha || '',
+      detalle: kardex.detalle || '',
+      valor_unitario: kardex.valor_unitario || '',
+      opcion: kardex.opcion || '',
+      cantidad: event.target.value
+    })
+  }
+
+  const sendData = (event) => {
+    event.preventDefault()
+    console.log(`
+      Enviaste los siguienes datos:
+        fecha: ${kardex.fecha}
+        detalle: ${kardex.detalle}
+        valor unitario: ${kardex.valor_unitario}
+        opcion: ${kardex.opcion}
+        cantidad: ${kardex.cantidad}
+    `)
+  }
 
   return (
     <div>
       <div>
         <h1>Formulario</h1>
-        <form action="" >
+        <form onSubmit={sendData} >
           <div className="ui form">
-            <div className="three fields" wfd-id="351">
-              <div className="field" wfd-id="356">
-                <label wfd-id="357">Fecha</label>
-                <input type="date" spellcheck="false" data-ms-editor="true" />
+            <div className="three fields">
+              <div className="field">
+                <label>Fecha</label>
+                <input 
+                  type="date"
+                  data-ms-editor="true"
+                  value={kardex.fecha}
+                  onChange={handleChangeDate} 
+                />
               </div>
               <div className="field">
                 <label>Detalles</label>
-                <input type="text" placeholder="Detalles" spellcheck="false" data-ms-editor="true" />
+                <input 
+                  type="text"
+                  placeholder="Detalles"
+                  data-ms-editor="true" 
+                  value={kardex.detalle}
+                  onChange={handleChangeDetails}
+                />
               </div>
               <div className="field">
                 <label>Valor Unitario</label>
-                <input type="number" placeholder="Valor" spellcheck="false" data-ms-editor="true" />
+                <input
+                  type="number"
+                  placeholder="Valor"
+                  data-ms-editor="true"
+                  value={kardex.valor_unitario}
+                  onChange={handleChangeValueUnitary}
+                />
               </div>  
             </div>
             <div className="two fields">
               <div className="field">
                 <label>Opcion</label>
-                <select name="" id="">
+                <select value={kardex.opcion} onChange={handleChangeOption}>
                   <option value="0">Seccionar una opcion...</option>
                   <option value="1">Entrada</option>
                   <option value="2">Salida</option>
@@ -33,12 +121,18 @@ const Formulario = () => {
               </div>
               <div className="field">
                 <label>Cantidad</label>
-                <input type="number" placeholder="Cantidad" spellcheck="false" data-ms-editor="true" />
+                <input
+                  type="number"
+                  placeholder="Cantidad"
+                  data-ms-editor="true"
+                  value={kardex.cantidad}
+                  onChange={handleChangeQuantity}
+                />
               </div>
             </div>
             <div className="three fields">
               <div className="field">
-                <button className="ui primary button">Guardar</button>
+                <button type="submit" className="ui primary button">Guardar</button>
               </div>
             </div>
           </div>
@@ -47,5 +141,7 @@ const Formulario = () => {
     </div>
   )
 }
+
+
 
 export default Formulario
